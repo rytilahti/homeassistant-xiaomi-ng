eb80f57e06c4759a57877e14fb969202efbbdfc0
Teemu Rytilahti <tpr@iki.fi>
2023-01-15 02:56:23 +0100

    Move XiaomiButton into button platform

--- custom_components/xiaomi_ng/button.py
+++ custom_components/xiaomi_ng/button.py
@@ -6,11 +6,57 @@ import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from typing import Callable
+
+from homeassistant.components.button import (
+    ButtonDeviceClass,
+    ButtonEntity,
+    ButtonEntityDescription,
+)
+from .device import XiaomiMiioEntity
+from homeassistant.helpers.entity import EntityCategory
+
 
 from .const import DOMAIN, KEY_COORDINATOR, KEY_DEVICE
-from .ng_button import XiaomiButton
 
 _LOGGER = logging.getLogger(__name__)
+
+
+class XiaomiButton(XiaomiMiioEntity, ButtonEntity):
+    """Representation of Xiaomi button."""
+
+    entity_description: ButtonEntityDescription
+    method: Callable
+
+    _attr_device_class = ButtonDeviceClass.RESTART
+    _attr_has_entity_name = True
+
+    def __init__(self, button, device, entry, coordinator):
+        """Initialize the plug switch."""
+        self._name = button.name
+        unique_id = f"{entry.unique_id}_button_{button.id}"
+        self.method = button.method
+
+        super().__init__(device, entry, unique_id, coordinator)
+
+        # TODO: This should always be CONFIG for settables and non-configurable?
+        category = EntityCategory(button.extras.get("entity_category", "config"))
+        description = ButtonEntityDescription(
+            key=button.id,
+            name=button.name,
+            icon=button.extras.get("icon"),
+            device_class=button.extras.get("device_class"),
+            entity_category=category,
+        )
+
+        self.entity_description = description
+
+    async def async_press(self) -> None:
+        """Press the button."""
+        await self._try_command(
+            f"Failed to execute button {self._name}",
+            self.method,
+        )
 
 
 async def async_setup_entry(

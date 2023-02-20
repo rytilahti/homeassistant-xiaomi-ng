46e1a3d7f78d852d2f22f7e87dc4a5c547fa8a74
Teemu Rytilahti <tpr@iki.fi>
2023-02-20 15:53:07 +0100

    Cleanup entity constructor, add get_value and get_descriptor based on standard identifiers

--- custom_components/xiaomi_miio/entity.py
+++ custom_components/xiaomi_miio/entity.py
@@ -2,18 +2,19 @@ import datetime
 import logging
 from enum import Enum
 from functools import partial
-from typing import Any, TypeVar
+from typing import Any, TypeVar, Optional, cast
 
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import CONF_MODEL
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity,
     DataUpdateCoordinator,
 )
-from miio import Device, DeviceException
+from miio import DeviceException
+from miio.descriptors import Descriptor, SettingType, EnumSettingDescriptor
+from miio.identifiers import StandardIdentifier
 
 from .const import DOMAIN
+from .device import XiaomiDevice
 
 _T = TypeVar("_T", bound=DataUpdateCoordinator[Any])
 
@@ -28,47 +29,42 @@ class XiaomiEntity(CoordinatorEntity[_T]):
 
     def __init__(
         self,
-        device: Device,
-        entry: ConfigEntry,
-        unique_id: str,
-        coordinator: DataUpdateCoordinator,
+        device: XiaomiDevice,
+        descriptor: Descriptor | None = None,  # main devices have no descriptor
     ):
         """Initialize the coordinated Xiaomi Miio Device."""
-        super().__init__(coordinator)
+        super().__init__(device.coordinator)
         self._device = device
-        self._model = entry.data[CONF_MODEL]
-        self._entry = entry
-        self._device_info = None
-        self._device_id = device.device_id
-        self._device_name = entry.title
-        self._attr_unique_id = unique_id
+        self._model = device.model
+        self._attr_unique_id = device.device_id
+        if descriptor is not None:
+            self._attr_unique_id += f"_{descriptor.id}"
         self._attr_available = True
 
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device info."""
         extras = {}
-        if self._device_info is not None:
-            extras["hw_version"] = self._device_info.hardware_version
-            extras["sw_version"] = self._device_info.firmware_version
+        if self._device.info is not None:
+            extras["hw_version"] = self._device.info.hardware_version
+            extras["sw_version"] = self._device.info.firmware_version
 
-        device_info = DeviceInfo(
-            identifiers={(DOMAIN, self._device_id)},
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._device.device_id)},
             default_manufacturer="Xiaomi",
-            model=self._model,
-            name=self._device_name,
+            model=self._device.model,
+            name=self._device.name,
             **extras,
         )
-
-        return device_info
 
     async def _try_command(self, mask_error, func, *args, **kwargs):
         """Call a miio device command handling error messages."""
         try:
             full_func = partial(func, *args, **kwargs)
+            _LOGGER.info("Calling %s with %s %s", full_func, args, kwargs)
             result = await self.hass.async_add_executor_job(full_func)
 
-            _LOGGER.debug("Response received from miio device: %s", result)
+            _LOGGER.info("Response received from miio device: %s", result)
 
             return True
         except DeviceException as exc:
@@ -81,7 +77,11 @@ class XiaomiEntity(CoordinatorEntity[_T]):
     @classmethod
     def _extract_value_from_attribute(cls, state, attribute):
         """Extract value from state."""
-        value = getattr(state, attribute)
+        try:
+            value = getattr(state, attribute)
+        except:
+            _LOGGER.error("Unable to read attribute %s from %s", attribute, state)
+            return None
 
         if isinstance(value, Enum):
             return value.value
@@ -97,22 +97,57 @@ class XiaomiEntity(CoordinatorEntity[_T]):
 
         return value
 
-    def get_setting(self, name: str):
-        """Get setting value."""
-        settings = self._device.settings()
-        return self._extract_value_from_attribute(
-            self.coordinator.data, settings[name].id
-        )
+    def get_descriptor(self, name: str | StandardIdentifier) -> Optional[Descriptor]:
+        """Get python-miio descriptor."""
+        if isinstance(name, StandardIdentifier):
+            name = name.value
 
-    def set_setting(self, name: str, value):
-        """Set setting to value."""
         settings = self._device.settings()
-        if name not in settings:
-            _LOGGER.warning("Device has no '%s'", name)
+        if name in settings:
+            return settings[name]
+
+        sensors = self._device.sensors()
+        if name in sensors:
+            return sensors[name]
+
+        return None
+
+
+    def get_value(self, name: str | StandardIdentifier):
+        """Get setting/sensor value."""
+        descriptor = self.get_descriptor(name)
+        if descriptor is None:
+            _LOGGER.error("Unable to find descriptor with name %s", name)
             return None
 
+        if Descriptor.Access.Read not in descriptor.access:
+            _LOGGER.debug("Tried to read %s, but it is not readable", name)
+            return None
+
+        try:
+            return self._extract_value_from_attribute(self.coordinator.data, descriptor.property)
+        except:
+            _LOGGER.error("Device has no '%s'", name)
+            return None
+
+    def set_setting(self, name: StandardIdentifier | str, value):
+        """Set setting to value."""
+        if isinstance(name, StandardIdentifier):
+            name = name.value
+        settings = self._device.settings()
+        if name not in settings:
+            _LOGGER.warning("Device has no '%s', available: %s", name, settings.keys())
+            breakpoint()
+            return None
+
+        descriptor = settings[name]
+        # TODO: this is not optimal.
+        if descriptor.setting_type == SettingType.Enum:
+            descriptor = cast(EnumSettingDescriptor, descriptor)
+            value = descriptor.choices[value].value
+
         _LOGGER.info("Going to set %s to %s", name, value)
-        return settings[name].setter(value)
+        return descriptor.setter(value)
 
     @staticmethod
     def _parse_time_delta(timedelta: datetime.timedelta) -> int:

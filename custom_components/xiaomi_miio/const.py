01bd2ffea43008d9e080611473a2fe31118f54eb
Teemu Rytilahti <tpr@iki.fi>
2023-02-20 15:51:12 +0100

    Cleanup consts

--- custom_components/xiaomi_miio/const.py
+++ custom_components/xiaomi_miio/const.py
@@ -11,31 +11,6 @@ CONF_CLOUD_PASSWORD = "cloud_password"
 CONF_CLOUD_COUNTRY = "cloud_country"
 CONF_USE_GENERIC = "use_genericmiot"
 
-# TODO: only used by the custom gateway, remove
-CONF_GATEWAY = "gateway"
-CONF_CLOUD_SUBDEVICES = "cloud_subdevices"
-
 # Keys
-KEY_COORDINATOR = "coordinator"
 KEY_DEVICE = "device"
 
-# Attributes
-ATTR_AVAILABLE = "available"
-
-# Status
-SUCCESS = ["ok"]
-
-
-# Exceptions
-class AuthException(Exception):
-    """Exception indicating an authentication error."""
-
-
-class SetupException(Exception):
-    """Exception indicating a failure during setup."""
-
-
-# Remote Services
-SERVICE_LEARN = "remote_learn_command"
-SERVICE_SET_REMOTE_LED_ON = "remote_set_led_on"
-SERVICE_SET_REMOTE_LED_OFF = "remote_set_led_off"

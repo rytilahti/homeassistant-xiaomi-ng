2028c40f5dab17ebcdf4dca80e86592403dacfaa
Teemu Rytilahti <tpr@iki.fi>
2024-03-14 00:02:26 +0100

    adapt device to upstream changes and use did for dummy mac

--- custom_components/xiaomi_miio/device.py
+++ custom_components/xiaomi_miio/device.py
@@ -1,20 +1,26 @@
 """Xiaomi device helper."""
 import logging
+from typing import TypeVar
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_MODEL
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-from miio import Device, DeviceInfo
-from miio.descriptors import (
+from miio import (
     ActionDescriptor,
     Descriptor,
-    SensorDescriptor,
-    SettingDescriptor,
+    DescriptorCollection,
+    Device,
+    DeviceException,
+    DeviceInfo,
+    PropertyDescriptor,
 )
-from miio.identifiers import FanId, StandardIdentifier, VacuumId
+from miio.identifiers import FanId, LightId, StandardIdentifier, VacuumId
 
 _LOGGER = logging.getLogger(__name__)
+
+
+T = TypeVar("T", bound=Descriptor)
 
 
 class XiaomiDevice:
@@ -33,49 +39,66 @@ class XiaomiDevice:
         self._config_data = config_entry.data
         self._device: Device = device
         self._coordinator: DataUpdateCoordinator = coordinator
-        self._device_info: DeviceInfo | None = None
+        self._device_info = None
 
     @property
     def name(self) -> str:
         return self._config_entry.title
 
-    def _filter_standard(self, descriptors) -> dict[str, Descriptor]:
+    def _filter_standard(
+        self, descriptors: DescriptorCollection[T]
+    ) -> DescriptorCollection[T]:
         """Filter out standard identifiers."""
-        identifier_classes = [StandardIdentifier, VacuumId, FanId]
+        identifier_classes = [StandardIdentifier, VacuumId, FanId, LightId]
         standard_identifiers = []
         for identifier_class in identifier_classes:
             standard_identifiers.extend(
                 [identifier.value for identifier in identifier_class]
             )
-        _LOGGER.error("Got standard identifiers: %s", standard_identifiers)
-        return {
-            key: value
-            for key, value in descriptors.items()
-            if key not in standard_identifiers
-        }
+        _LOGGER.debug("Got standard identifiers: %s", standard_identifiers)
+        # TODO: avoid constructing a new devicecollection
+        return DescriptorCollection(
+            {
+                descriptor.id: descriptor
+                for descriptor in descriptors.values()
+                if descriptor.id not in standard_identifiers
+            },
+            device=self._device,
+        )
 
-    def settings(self, skip_standard=False) -> dict[str, SettingDescriptor]:
+    def descriptors(self, skip_standard=False) -> DescriptorCollection[Descriptor]:
+        """Return all available descriptors, keyed with id."""
+        descriptors = self._device.descriptors()
+        if skip_standard:
+            descriptors = self._filter_standard(descriptors)
+
+        return descriptors  # noqa: RET504
+
+    def settings(self, skip_standard=False) -> DescriptorCollection[PropertyDescriptor]:
         """Return all available settings, keyed with id."""
         settings = self._device.settings()
         if skip_standard:
             settings = self._filter_standard(settings)
-        return {setting.id: setting for name, setting in settings.items()}
 
-    def sensors(self, skip_standard=False) -> dict[str, SensorDescriptor]:
+        return settings  # noqa: RET504
+
+    def sensors(self, skip_standard=False) -> DescriptorCollection[PropertyDescriptor]:
         """Return all available sensors, keyed with id."""
         sensors = self._device.sensors()
         if skip_standard:
             sensors = self._filter_standard(sensors)
-        return {sensor.id: sensor for name, sensor in sensors.items()}
 
-    def actions(self, skip_standard=False) -> dict[str, ActionDescriptor]:
+        return sensors  # noqa: RET504
+
+    def actions(self, skip_standard=False) -> DescriptorCollection[ActionDescriptor]:
         """Return all available actions, keyed with id."""
         actions = self._device.actions()
         if skip_standard:
             actions = self._filter_standard(actions)
-        return {action.id: action for name, action in actions.items()}
 
-    def action(self, name: StandardIdentifier | str):
+        return actions  # noqa: RET504
+
+    def get_method_for_action(self, name: StandardIdentifier | str):
         """Return action callable by name."""
         if isinstance(name, StandardIdentifier):
             name = name.value
@@ -87,9 +110,9 @@ class XiaomiDevice:
         return self._device
 
     @property
-    def device_id(self) -> str:
+    def device_id(self) -> int:
         """Return the device ID as string."""
-        return str(self._device.device_id)
+        return self._device.device_id
 
     @property
     def model(self) -> str:
@@ -97,30 +120,49 @@ class XiaomiDevice:
         return self._config_data[CONF_MODEL]
 
     @property
-    def detected_model(self) -> str | None:
-        assert self._device_info is not None
-        return self._device_info.model
-
-    @property
     def coordinator(self) -> DataUpdateCoordinator:
         return self._coordinator
 
     @property
     def info(self) -> DeviceInfo:
-        """Return the class containing device info."""
+        """Return the class containing device info.
+
+        TODO: This is misplaced and will request miIO.info from the device...
+        """
+        if self._device_info is None:
+            try:
+                self._device_info = self._device.info()
+            except DeviceException:
+                _LOGGER.error(
+                    "Unable to query info from device, returning dummy information"
+                )
+                assert self.model is not None
+
+                did_mac = f"ca:fe:{self.device_id:08x}"
+                self._device_info = DeviceInfo(
+                    {
+                        "model": self.model,
+                        "hw_ver": "dummy",
+                        "fw_ver": "dummy",
+                        "mac": did_mac.lower(),
+                    }
+                )
+
         return self._device_info
 
-    def get(self, key: str | StandardIdentifier):
+    def get(self, key: str | StandardIdentifier) -> PropertyDescriptor | None:
         """Return sensor/setting descriptor."""
         if isinstance(key, StandardIdentifier):
             key = key.value
-        if key in self.sensors():
-            return self.sensors()[key]
-        if key in self.settings():
-            return self.settings()[key]
 
-        _LOGGER.debug("Unable to find identifier: %s", key)
-        return None
+        props = self._device.descriptors()
+        try:
+            descriptor = props[key]
+        except KeyError:
+            _LOGGER.error("Unable to find descriptor '%s' for %s", key, self._device)
+            return None
+
+        return descriptor
 
     def fetch_info(self):
         """Fetch device info."""
